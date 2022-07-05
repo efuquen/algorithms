b29918d4c526d3bb4cf95e14fd606556c934888a
Edwin Fuquen <efuquen@gmail.com>
2022-07-04 20:58:00 -0600

    switch to object based draw

--- cmd/test/draw_client.go
+++ cmd/test/draw_client.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/efuquen/algorithms/pkg/algs4/draw"
 	"github.com/efuquen/algorithms/pkg/algs4/random"
 	"math"
 )
 
 func randomBarGraph() {
+	d := draw.NewDraw(draw.DefaultWindowTitle)
 	n := 50
 	var a []float64
 	for i := 0; i < n; i++ {
@@ -18,26 +18,26 @@ func randomBarGraph() {
 		y := float32(a[i] / 2.0)
 		rw := 0.5 / float32(n)
 		rh := float32(a[i] / 2.0)
-		fmt.Printf("x: %f y: %f rw: %f rh: %f\n", x, y, rw, rh)
-		draw.FilledRectangle(x, y, rw, rh)
+		d.FilledRectangle(x, y, rw, rh)
 	}
 }
 
 func pointGraphs() {
+	d := draw.NewDraw()
 	n := 100
-	draw.SetXScale(0.0, float32(n))
-	draw.SetYScale(0.0, float32(n*n))
-	draw.SetPenRadius(0.01)
+	d.SetXScale(0.0, float32(n))
+	d.SetYScale(0.0, float32(n*n))
+	d.SetPenRadius(0.01)
 
 	for i := float32(1); i <= float32(n); i++ {
-		draw.Point(i, i)
-		draw.Point(i, i*i)
-		draw.Point(i, i*float32(math.Log(float64(i))))
+		d.Point(i, i)
+		d.Point(i, i*i)
+		d.Point(i, i*float32(math.Log(float64(i))))
 	}
 }
 
 func main() {
 	//randomBarGraph()
 	pointGraphs()
-	draw.ShowAndRun()
+	draw.Run()
 }
--- pkg/algs4/draw/draw.go
+++ pkg/algs4/draw/draw.go
@@ -15,6 +15,9 @@ type myTheme struct{}
 var _ fyne.Theme = (*myTheme)(nil)
 
 func (m myTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameBackground {
+		return White
+	}
 	return theme.DefaultTheme().Color(name, variant)
 }
 func (m myTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
@@ -36,112 +39,128 @@ const DefaultSize float32 = 512
 const DefaultPenRadius float32 = 0.002
 
 var Black color.Color = color.Black
+var White color.Color = color.White
 var Blue color.Color = color.RGBA{R: 0, G: 0, B: 255, A: 255}
 var Red color.Color = color.RGBA{R: 255, G: 0, B: 0, A: 255}
 var Green color.Color = color.RGBA{R: 0, G: 255, B: 0, A: 255}
 
 var DefaultPenColor color.Color = Black
 
-const Border = 0.00
 const DefaultXMin = 0.0
 const DefaultXMax = 1.0
 const DefaultYMin = 0.0
 const DefaultYMax = 1.0
 
-var xmin, xmax, ymin, ymax float32
-
-var penRadius = DefaultPenRadius
-var penColor color.Color = DefaultPenColor
-var width = DefaultSize
-var height = DefaultSize
-
 var a = app.New()
-var w = a.NewWindow(DefaultWindowTitle)
-var content = container.NewWithoutLayout()
 
 func init() {
-	w.Resize(fyne.NewSize(DefaultSize, DefaultSize))
-	w.CenterOnScreen()
-	w.SetContent(content)
 	a.Settings().SetTheme(&myTheme{})
-
-	SetXScale(DefaultXMin, DefaultXMax)
-	SetYScale(DefaultYMin, DefaultYMax)
 }
 
-func SetPenRadius(r float32) {
-	penRadius = r
+type Draw struct {
+	window                 fyne.Window
+	content                *fyne.Container
+	penRadius              float32
+	penColor               color.Color
+	width                  float32
+	height                 float32
+	xmin, xmax, ymin, ymax float32
 }
 
-func SetPenColor(color color.Color) {
-	penColor = color
+func Run() {
+	a.Run()
 }
 
-func SetXScale(min float32, max float32) {
-	size := max - min
-	xmin = min - Border*size
-	xmax = max + Border*size
+func NewDraw(title string) Draw {
+	d := Draw{
+		a.NewWindow(title),
+		container.NewWithoutLayout(),
+		DefaultPenRadius,
+		DefaultPenColor,
+		DefaultSize,
+		DefaultSize,
+		DefaultXMin,
+		DefaultXMax,
+		DefaultYMin,
+		DefaultYMax,
+	}
+	d.window.Resize(fyne.NewSize(DefaultSize, DefaultSize))
+	d.window.CenterOnScreen()
+	d.window.SetContent(d.content)
+
+	return d
 }
 
-func SetYScale(min float32, max float32) {
-	size := max - min
-	ymin = min - Border*size
-	ymax = max + Border*size
+func (d *Draw) SetPenRadius(r float32) {
+	d.penRadius = r
 }
 
-func scaleX(x float32) float32 {
-	return width * (x - xmin) / (xmax - xmin)
+func (d *Draw) SetPenColor(color color.Color) {
+	d.penColor = color
 }
 
-func scaleY(y float32) float32 {
-	return height * (ymax - y) / (ymax - ymin)
+func (d *Draw) SetXScale(min float32, max float32) {
+	d.xmin = min
+	d.xmax = max
 }
 
-func factorX(w float32) float32 {
-	return w * width / float32(math.Abs(float64(xmax-xmin)))
+func (d *Draw) SetYScale(min float32, max float32) {
+	d.ymin = min
+	d.ymax = max
 }
 
-func factorY(h float32) float32 {
-	return h * height / float32(math.Abs(float64(ymax-ymin)))
+func (d *Draw) scaleX(x float32) float32 {
+	return d.width * (x - d.xmin) / (d.xmax - d.xmin)
 }
 
-func Point(x, y float32) {
-	xs := scaleX(x)
-	ys := scaleY(y)
-	scaledPenRadius := penRadius * DefaultSize
+func (d *Draw) scaleY(y float32) float32 {
+	return d.height * (d.ymax - y) / (d.ymax - d.ymin)
+}
 
-	circle := canvas.NewCircle(penColor)
+func (d *Draw) factorX(w float32) float32 {
+	return w * d.width / float32(math.Abs(float64(d.xmax-d.xmin)))
+}
+
+func (d *Draw) factorY(h float32) float32 {
+	return h * d.height / float32(math.Abs(float64(d.ymax-d.ymin)))
+}
+
+func (d *Draw) Point(x, y float32) {
+	xs := d.scaleX(x)
+	ys := d.scaleY(y)
+	scaledPenRadius := d.penRadius * DefaultSize
+
+	circle := canvas.NewCircle(d.penColor)
 	circle.Move(fyne.NewPos(xs-scaledPenRadius/2, ys-scaledPenRadius/2))
 	circle.Resize(fyne.NewSize(scaledPenRadius, scaledPenRadius))
-	content.Add(circle)
+	d.content.Add(circle)
+	d.window.Show()
 }
 
-func Circle(x, y, radius float32) {
-	xs := scaleX(x)
-	ys := scaleY(y)
-	ws := factorX(2 * radius)
-	hs := factorY(2 * radius)
+func (d *Draw) Circle(x, y, radius float32) {
+	xs := d.scaleX(x)
+	ys := d.scaleY(y)
+	ws := d.factorX(2 * radius)
+	hs := d.factorY(2 * radius)
 
 	circle := canvas.NewCircle(color.Transparent)
-	circle.StrokeColor = penColor
-	circle.StrokeWidth = penRadius * DefaultSize
+	circle.StrokeColor = d.penColor
+	circle.StrokeWidth = d.penRadius * DefaultSize
 	circle.Move(fyne.NewPos(xs-ws/2, ys-hs/2))
 	circle.Resize(fyne.NewSize(ws, hs))
-	content.Add(circle)
+	d.content.Add(circle)
+	d.window.Show()
 }
 
-func FilledRectangle(x, y, halfWidth, halfHeight float32) {
-	xs := scaleX(x)
-	ys := scaleY(y)
-	ws := factorX(2 * halfWidth)
-	hs := factorY(2 * halfHeight)
+func (d *Draw) FilledRectangle(x, y, halfWidth, halfHeight float32) {
+	xs := d.scaleX(x)
+	ys := d.scaleY(y)
+	ws := d.factorX(2 * halfWidth)
+	hs := d.factorY(2 * halfHeight)
 
-	rect := canvas.NewRectangle(penColor)
+	rect := canvas.NewRectangle(d.penColor)
 	rect.Move(fyne.NewPos(xs-ws/2, ys-hs/2))
 	rect.Resize(fyne.NewSize(ws, hs))
-	content.Add(rect)
-}
-
-func ShowAndRun() {
-	w.ShowAndRun()
+	d.content.Add(rect)
+	d.window.Show()
 }
